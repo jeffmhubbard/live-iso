1076695fd7cd8d5bd1f0771bfc38e061df095fc3
Jeff M. Hubbard <jeffmhubbard@gmail.com>
2026-02-13 09:07:28 -0600

    init

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # live-iso
 PENGUIN Live Desktop and Installer
+
+## Update pacman.conf
+
+    ...
+    [penguin]
+    Server = https://repo.penguin.fyi/$repo/$arch
+    ...
+
+
+## Create ISO
+
+    cd live-iso
+    sudo mkarchiso -rv penguin
+
---
+++ penguin/profiledef.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+
+iso_name="penguin"
+iso_label="PENG_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
+iso_publisher="PENGUIN <https://penguin.fyi>"
+iso_application="PENGUIN Live/Rescue DVD"
+iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
+install_dir="arch"
+buildmodes=('iso')
+bootmodes=('bios.syslinux' 'uefi.systemd-boot')
+pacman_conf="pacman.conf"
+airootfs_image_type="squashfs"
+airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
+file_permissions=(
+  ["/etc/shadow"]="0:0:400"
+  ["/etc/gshadow"]="0:0:400"
+  ["/root"]="0:0:750"
+  ["/etc/polkit-1/rules.d"]="0:0:750"
+  ["/etc/sudoers.d"]="0:0:750"
+  ["/etc/sudoers.d/g_wheel"]="0:0:440"
+  ["/root/.automated_script.sh"]="0:0:755"
+  ["/root/.gnupg"]="0:0:700"
+  ["/usr/local/bin/choose-mirror"]="0:0:755"
+  ["/usr/local/bin/livecd-sound"]="0:0:755"
+)
